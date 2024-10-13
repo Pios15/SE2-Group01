11ba79671de5dd6c7573760daf41368458c04ba4
Francesca Villanova <s329950@studenti.polito.it>
2024-10-13 16:41:13 +0200

    Merge of dao functions and update of index.mjs

--- server/DAO/ticketDAO.mjs
+++ server/DAO/ticketDAO.mjs
@@ -1,6 +1,7 @@
 import QRCode from 'qrcode';
 
 import db from '../db.mjs';
+import { Ticket } from '../model.mjs';
 
 export default function TicketDAO() {
   //this function returns a link to the page which makes you download the ticket
@@ -31,7 +32,7 @@ export default function TicketDAO() {
 
   //this function returns all the data to be shown on the ticket
   //receives the service
-  this.getTicket = id_service => {
+  /*this.getTicket = id_service => {
     //change db for having a parameter service for better queries
     //add date for having every day a new line
 
@@ -81,6 +82,90 @@ export default function TicketDAO() {
           });
         }
       });
+    });
+  };*/
+
+  //this function computes the next ticket number for a given service
+  this.compute_next_ticket_number = id_service => {
+    return new Promise((resolve, reject) => {
+      db.get(
+        'SELECT ticket_number FROM tickets WHERE service = ? ORDER BY id DESC LIMIT 1',
+        [id_service],
+        (error, row) => {
+          if (error) {
+            reject(error);
+          } else {
+            const serviceLetterMap = {
+              1: 'A',
+              2: 'B',
+              3: 'C',
+              4: 'D',
+            };
+            const lastTicketNumber = row
+              ? parseInt(row.ticket_number.slice(0, -1))
+              : 0;
+            resolve(`${lastTicketNumber + 1}${serviceLetterMap[id_service]}`);
+          }
+        },
+      );
+    });
+  };
+
+  //this function creates a new ticket
+  this.getTicket = async id_service => {
+    const ticket_number = await this.compute_next_ticket_number(id_service);
+    const status = 'Waiting';
+    const estimated_time = 0; //va preso dalla funzione
+
+    return new Promise((resolve, reject) => {
+      db.run(
+        'INSERT INTO tickets (ticket_number, estimated_time, status, service) VALUES (?, ?, ?, ?)',
+        [ticket_number, estimated_time, status, id_service],
+        function (error) {
+          if (error) {
+            reject(error);
+          } else {
+            const id = this.lastID;
+            const ticketData = new Ticket(
+              id,
+              ticket_number,
+              estimated_time,
+              status,
+              id_service,
+            );
+            const url = `http://localhost:3000/ticket/${ticketData.ticket_id}`;
+            QRCode.toDataURL(url, (err, qrCodeUrl) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve({ ticketData, qrCodeUrl });
+              }
+            });
+          }
+        },
+      );
+    });
+  };
+
+  //this function changes the status of a ticket (served, canceled, in progress)
+  this.change_status = (ticket_number, new_status_code) => {
+    return new Promise((resolve, reject) => {
+      const statusMap = {
+        1: 'Served',
+        2: 'Canceled',
+        3: 'In_Progress',
+      };
+      db.run(
+        'UPDATE tickets SET status = ? WHERE ticket_number = ?',
+        [statusMap[new_status_code], ticket_number],
+        error => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(statusMap[new_status_code]);
+          }
+        },
+      );
     });
   };
 }
