7e80f6120422da5a7ce46bb2c66e74c37dc94696
Francesca Villanova <s329950@studenti.polito.it>
2024-10-14 13:40:31 +0200

    Update functions with updated attributes of database

--- server/model.mjs
+++ server/model.mjs
@@ -1,13 +1,5 @@
-function Ticket(
-  id,
-  ticket_number,
-  estimated_time,
-  status = 'Waiting',
-  service,
-) {
-  this.id = id;
+function Ticket(ticket_number, status = 'Waiting', service) {
   this.ticket_number = ticket_number;
-  this.estimated_time = estimated_time;
   this.status = status;
   this.service = service;
 }
