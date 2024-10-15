1552ccfa10ed7942fb8e70178481c3a138c26b0e
criicky <scan.riccardo@gmail.com>
2024-10-15 14:31:57 +0200

    new UI for displaying the call and creating a separate components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([{ path: '/*', element: <App /> }]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
