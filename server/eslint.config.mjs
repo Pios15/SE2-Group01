6e30d52d93f47cb69fb9cf2360a8cadbd0d9c46b
GIUSEPPE BARONE s332157 <s332157@studenti.polito.it>
2024-10-16 16:40:20 +0200

    feat: test configuration

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -3,6 +3,26 @@ import pluginJs from '@eslint/js';
 import globals from 'globals';
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: globals.node, // Solo ambiente Node.js
+    },
+  },
   pluginJs.configs.recommended,
 ];
+
+/**
+ *
+ * import pluginJs from '@eslint/js';
+ * import globals from 'globals';
+ *
+ * export default [
+ *   {
+ *     languageOptions: {
+ *       globals: globals.node,  // Solo ambiente Node.js
+ *     },
+ *   },
+ *   pluginJs.configs.recommended,
+ * ];
+ *
+ */
